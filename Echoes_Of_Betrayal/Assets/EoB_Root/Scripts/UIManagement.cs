a6ae453e4852d171286f7962b5add75e4e468b75
carlescarles05 <151786310+carlescarles05@users.noreply.github.com>
2024-09-25 13:09:37 +0200

    mec

--- Echoes_Of_Betrayal/Assets/EoB_Root/Scripts/UIManagement.cs
+++ Echoes_Of_Betrayal/Assets/EoB_Root/Scripts/UIManagement.cs
@@ -7,7 +7,7 @@ using static UnityEngine.AudioSettings;
 public class UIManagement : MonoBehaviour
 {
 
-    public GameManager gameManager;
+    /*public GameManager gameManager;
 
 
     // Update is called once per frame
@@ -34,6 +34,55 @@ public class UIManagement : MonoBehaviour
     public void MOVIL()
     {
         gameManager.movil = true;
+    }*/
+
+    // Asignar el GameManager desde el Inspector
+    public GameManager gameManager;
+
+    void Start()
+    {
+        // Si el GameManager no se asignó en el Inspector, lo busca en la escena
+        if (gameManager == null)
+        {
+            gameManager = FindObjectOfType<GameManager>(); // Busca el GameManager en toda la escena
+        }
+
+        if (gameManager == null)
+        {
+            Debug.LogError("GameManager no encontrado. Asegúrate de que está en la escena.");
+        }
     }
-   
+
+    public void PEREZOSO()
+    {
+        if (gameManager != null)
+        {
+            gameManager.perezoso = true;
+        }
+    }
+
+    public void VASO()
+    {
+        if (gameManager != null)
+        {
+            gameManager.vaso = true;
+        }
+    }
+
+    public void LLAVES()
+    {
+        if (gameManager != null)
+        {
+            gameManager.llaves = true;
+        }
+    }
+
+    public void MOVIL()
+    {
+        if (gameManager != null)
+        {
+            gameManager.movil = true;
+        }
+    }
+
 }
