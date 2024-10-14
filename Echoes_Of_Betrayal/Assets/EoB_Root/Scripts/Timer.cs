44e6f12a447be114a80c7cb4593c87f0505679e8
carlescarles05 <151786310+carlescarles05@users.noreply.github.com>
2024-10-14 18:02:37 +0200

    timer hecho

---
+++ Echoes_Of_Betrayal/Assets/EoB_Root/Scripts/Timer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;   
+    public float totalTime = 300f; 
+    private float currentTime;
+
+    void Start()
+    {
+        currentTime = totalTime; 
+    }
+
+    void Update()
+    {
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime; 
+            UpdateTimerUI();
+        }
+        else
+        {
+            currentTime = 0;
+            TimerEnded();
+        }
+    }
+
+    void UpdateTimerUI()
+    {
+        // Formato de tiempo (minutos:segundos)
+        int minutes = Mathf.FloorToInt(currentTime / 60);
+        int seconds = Mathf.FloorToInt(currentTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void TimerEnded()
+    {
+        Debug.Log("¡El tiempo se ha acabado!");
+    }
+}
