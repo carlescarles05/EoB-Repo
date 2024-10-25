83d3cab9fd19a8a05eabba6f3573c2d74bd1ac03
EduardBeltranLopez <edubeltranlp@gmail.com>
2024-10-25 09:07:14 +0200

    yayayayaya

--- Echoes_Of_Betrayal/Assets/EoB_Root/Scripts/CharacterManagement.cs
+++ Echoes_Of_Betrayal/Assets/EoB_Root/Scripts/CharacterManagement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CharacterManagement : MonoBehaviour
 {
 
+    public CameraSwipe cameraSwipe;
+
     public GameObject Powa;
     public GameObject Rick;
     public GameObject Mery;
@@ -13,6 +15,11 @@ public class CharacterManagement : MonoBehaviour
 
     public GameObject contenedorCharacter;
 
+
+    private void Start()
+    {
+        cameraSwipe = GetComponent<CameraSwipe>();
+    }
 
     private void Update()
     {
@@ -24,58 +31,65 @@ public class CharacterManagement : MonoBehaviour
 
     public void POWA()
     {
-            Powa.SetActive(true);
+        Powa.SetActive(true);
+        cameraSwipe.cameraSpeed = 0f;
     }
 
     public void POWACLOSE()
     {
         Powa.SetActive(false);
         GameManager.Instance.powa = true;
+        cameraSwipe.cameraSpeed = 0.5f;
     }
 
     public void RICK()
     {
-            Rick.SetActive(true);
-        
+        Rick.SetActive(true);
+        cameraSwipe.cameraSpeed = 0f;
+
     }
     public void RICKCLOSE()
     {
         Rick.SetActive(false);
         GameManager.Instance.rickDialogueClose = true;
         GameManager.Instance.rick = true;
+        cameraSwipe.cameraSpeed = 0.5f;
     }
 
     public void MERY()
     {
         Mery.SetActive(true);
-        
+        cameraSwipe.cameraSpeed = 0f;
     }
     public void MERYCLOSE()
     {
         Mery.SetActive(false);
         GameManager.Instance.mery = true;
+        cameraSwipe.cameraSpeed = 0.5f;
     }
 
     public void FRAN()
     {
         Fran.SetActive(true);
-        
+        cameraSwipe.cameraSpeed = 0f;
     }
     public void FRANCLOSE()
     {
         Fran.SetActive(false);
         GameManager.Instance.fran = true;
+        cameraSwipe.cameraSpeed = 0.5f;
     }
 
     public void KIREI()
     {
         Kirei.SetActive(true);
-        
+        cameraSwipe.cameraSpeed = 0f;
     }
     public void KIREICLOSE()
     {
         Kirei.SetActive(false);
         GameManager.Instance.kirei = true;
+        cameraSwipe.cameraSpeed = 0.5f;
     }
 
 }
