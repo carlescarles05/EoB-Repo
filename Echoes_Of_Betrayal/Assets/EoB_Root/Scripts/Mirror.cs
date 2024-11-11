9c8d84547c9fc8b6ba0bc88338121d9b62da719c
EduardBeltranLopez <edubeltranlp@gmail.com>
2024-11-11 17:38:39 +0100

    mirror pc

--- Echoes_Of_Betrayal/Assets/EoB_Root/Scripts/Mirror.cs
+++ Echoes_Of_Betrayal/Assets/EoB_Root/Scripts/Mirror.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public class Mirror : MonoBehaviour
@@ -55,6 +55,71 @@ public class Mirror : MonoBehaviour
         }
     }
     
+}*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour
+{
+    [SerializeField]
+    private Transform mirrorPlace;
+
+    private Vector2 initialPosition;
+
+    private float deltaX, deltaY;
+
+    public bool locked;
+
+    private Collider2D objectCollider;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+        objectCollider = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        if (!locked)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Detecta si el objeto fue clicado
+                if (objectCollider == Physics2D.OverlapPoint(mousePos))
+                {
+                    deltaX = mousePos.x - transform.position.x;
+                    deltaY = mousePos.y - transform.position.y;
+                }
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                // Mueve el objeto con el ratón si se mantiene presionado el botón izquierdo
+                if (objectCollider == Physics2D.OverlapPoint(mousePos))
+                {
+                    transform.position = new Vector2(mousePos.x - deltaX, mousePos.y - deltaY);
+                }
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                // Suelta el objeto y verifica si debe bloquearse en el lugar objetivo
+                if (Mathf.Abs(transform.position.x - mirrorPlace.position.x) <= 0.5f &&
+                    Mathf.Abs(transform.position.y - mirrorPlace.position.y) <= 0.5f)
+                {
+                    transform.position = new Vector2(mirrorPlace.position.x, mirrorPlace.position.y);
+                    locked = true;
+                }
+                else
+                {
+                    transform.position = initialPosition;
+                }
+            }
+        }
+    }
 }
 
-    
+
